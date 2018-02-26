e27a8b61d09ee2318c634ca88ba928b3bb67edd0
Oriol Manzano <oriol.manzano@epitech.eu>
2018-02-26 15:37:19 +0100

    j

--- map_creation.c
+++ map_creation.c
@@ -29,6 +29,18 @@ void	print_trees(sfVector2i *value, sfVector2f **map2d, sfVector2f **water, luis
 	}		
 }
 
+int	mouse_tree_press(int num, sfVector2i *value, int x, int y)
+{
+	if (sfMouse_isButtonPressed(sfMouseLeft)) {
+		value[num].x = x;
+		value[num].y = y;
+		num++;
+		value[num].x = -84;
+		value[num].y = -84;
+	}
+	return (num);
+}
+
 sfVector2i	*put_tree_all_square(sfVector2f **map2d, sfVector2f **water, luis *a, sfVector2i click)
 {
 	int		x;
@@ -48,14 +60,7 @@ sfVector2i	*put_tree_all_square(sfVector2f **map2d, sfVector2f **water, luis *a,
 			    click.y >= water[y][x].y &&
 			    click.y <= water[y + 1][x].y) {
 				//printf("x:%d,y;%d, water[x]:%f,water[y]:%f\n", x, y, water[y][x].x, water[y][x].y);
-				//printf("clickx:%d,y:%d\n",click.x, click.y);
-				if (sfMouse_isButtonPressed(sfMouseLeft)) {
-					value[num].x = x;
-					value[num].y = y;
-					num++;
-					value[num].x = -84;
-					value[num].y = -84;
-				}
+				num = mouse_tree_press(num, value, x, y); 
 				put_tree_in_place(map2d, water, y, x);
 				draw_2d_map(a->window, map2d, water);
 			}
@@ -71,17 +76,13 @@ int	map_creation(int **water_map, int **map, luis *a)
 	sfVector2f      **map2d;
 	sfVector2f      **water;
 	sfVector2i      click;
-	//sfVector2i	*value;
-	
+		
 	click = sfMouse_getPosition(a->screen);
 	//printf("mouse - %d %d\n", click.x, click.y);
 	water = create_2d_water_map(water_map);
 	map2d = create_2d_map(map);
 	put_tree_all_square(map2d, water, a, click);
-	//print_trees(value, map2d, water, a);
-	//draw_2d_map(a->window, map2d, water);
 	draw_2d_water_map(a->window, water);
-	//upgrade_the_position(click, map2d);                   
 	free_map2d(map2d);
 	free_map2d(water);
 	return (0);
